db007e92ba2b2376e3176869395595993dddf605
ajsm <codingportugal@gmail.com>
2021-06-14 17:47:12 +0100

    4 - Criação de Models, Controllers, Views, Routes => até LOGIN

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,46 @@
 |
 */
 
+
+Route::group(['middleware'=>['auth'], 'namespace'=>'Admin','prefix'=>'admin'], function() {
+
+
+	Route::get('balance', 'BalanceController@index')->name('admin.balance');
+
+	Route::get('/','AdminController@index')->name('admin.home');
+
+
+
+});
+
+
+
+Route::get('/','Site\SiteController@index')->name('home');
+
+
+
+
+
+
+
+
+
+
+
+
+
+Auth::routes();
+
+
+
+
+
+
+
+
+
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
