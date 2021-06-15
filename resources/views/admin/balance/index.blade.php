782da1891d523e3bc65e33b9ad5ace6249461198
ajsm <codingportugal@gmail.com>
2021-06-15 15:41:52 +0100

    10 - admin/balance/index.php

--- resources/views/admin/balance/index.blade.php
+++ resources/views/admin/balance/index.blade.php
@@ -12,10 +12,23 @@
 @stop
 
 
+@section('content')
 
+	<div class="col-lg-3 col-6">
+	<!-- small box -->
+	<div class="small-box bg-success">
+	  <div class="inner">
+	    <h3>R${{ number_format($amount,2,'.','') }}<sup style="font-size: 20px"></sup></h3>
 
+	    <p>Saldo</p>
+	  </div>
+	  <div class="icon">
+	    <i class="ion ion-stats-bars"></i>
+	  </div>
+	  <a href="#" class="small-box-footer">Histórico <i class="fas fa-arrow-circle-right"></i></a>
+	</div>
+</div>
 
-
-
+@stop
 
 
